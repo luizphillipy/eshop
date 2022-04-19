6fe960b4f87d93259bc6d0e01c92701d7965a14e
Luiz Sampaio <lsampaio@miu.edu>
2022-04-19 13:42:17 -0500

    database test data

--- src/main/java/edu/miu/cs/cs425/eshop/config/TestConfig.java
+++ src/main/java/edu/miu/cs/cs425/eshop/config/TestConfig.java
@@ -1,2 +1,26 @@
-package edu.miu.cs.cs425.eshop.config;public class TestConfig {
+package edu.miu.cs.cs425.eshop.config;
+
+import edu.miu.cs.cs425.eshop.model.User;
+import edu.miu.cs.cs425.eshop.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+        User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+        userRepository.save(u1);
+        userRepository.save(u2);
+    }
+
+
 }
--- src/main/java/edu/miu/cs/cs425/eshop/repository/UserRepository.java
+++ src/main/java/edu/miu/cs/cs425/eshop/repository/UserRepository.java
@@ -2,6 +2,8 @@ package edu.miu.cs.cs425.eshop.repository;
 
 import edu.miu.cs.cs425.eshop.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
 }
